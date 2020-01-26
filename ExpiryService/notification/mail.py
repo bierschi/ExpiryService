0c1f6715b86d6575cbbeccc62d2abda98316d219
Christian Bierschneider <christian.bierschneider@web.de>
2020-01-26 16:11:12 +0100

    Updated the mail class

--- ExpiryService/notification/mail.py
+++ ExpiryService/notification/mail.py
@@ -8,18 +8,17 @@ class Mail:
     """ class Mail to provide sending expiry notification to specific email address
 
     USAGE:
-            mail = Mail(smtp_server="smtp.web.de", port=587, sender=sender@web.de, receiver=receiver@web.de, debug=False)
+            mail = Mail(smtp_server="smtp.web.de", port=587, debug=False)
             mail.login(username='', password='')
             mail.send()
     """
-    def __init__(self, smtp_server, port, sender, debug=False):
+    def __init__(self, smtp_server, port, debug=False):
         self.logger = logging.getLogger('ExpiryService')
         self.logger.info('create class Mail')
 
         self.smtp_server = smtp_server
         self.port = port
-        self.sender = sender
-
+        self.sender = None
         self.server = smtplib.SMTP(self.smtp_server, self.port)
         self.context = ssl.create_default_context()
         self.server.starttls(context=self.context)
@@ -28,7 +27,6 @@ class Mail:
             self.server.set_debuglevel(True)
 
         self.msg = EmailMessage()
-        self.msg['From'] = self.sender
 
     def __del__(self):
         """ destructor
@@ -41,7 +39,9 @@ class Mail:
         """ login to email account with username and password
 
         """
-        self.server.login(user=username, password=password)
+        self.sender = username
+        self.msg['From'] = self.sender
+        self.server.login(user=self.sender, password=password)
 
     def set_subject(self, subject):
         """ sets the subject for the email
